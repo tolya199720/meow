b8ff3450e6c7777b867f844beec40595deaaee4a
tolya199720 <tolya199720@gmail.com>
2024-07-24 18:10:05 +0300

    1.02

--- HelloGit/HelloGit/Program.cs
+++ HelloGit/HelloGit/Program.cs
@@ -8,9 +8,17 @@ namespace HelloGit
 {
     internal class Program
     {
+       static public int PArse(int a, int b)
+        {
+            return a + b;
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("hello git");
+            int f, b;
+            f = int.Parse(Console .ReadLine());
+            b = int.Parse(Console.ReadLine());
+            Console.WriteLine("сума:"+ PArse(f, b));
         }
     }
 }
